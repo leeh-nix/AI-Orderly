ae648429b61ffe4a7e1f0ea7ee74b1a9f8d8c41d
nksonkar01 <nksonkar01@gmail.com>
2024-05-23 11:02:30 +0000

    logs

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 import os
 from flask import Flask, request, jsonify, send_file
+import requests
+# from gemini import sendResponse
+
 
 app = Flask(__name__)
 
+# DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 VERIFY_TOKEN = "omgslay"
-@app.route("/")
-# def index():
+
+@app.route("/", methods=['GET', 'POST'])
+def name(req, res):
+    print(req.body)
+    return "Hello World"
+
+@app.route("/webhook", methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         verify_token = request.args.get('hub.verify_token')
@@ -18,9 +27,17 @@ def index():
         data = request.get_json()
         print("Received webhook data:", data)
         # Here you can process the incoming message data
+
+        # Get response from Gemini API
+        # response_text = sendResponse(user_message)
+        # Send response to Discord
+        # send_message_to_discord(response_text)
+
         return jsonify({'status': 'success'}), 200
 
     return send_file('index.html')
+
+
 def send_message_to_discord(message):
     payload = {"content": message}
     headers = {"Content-Type": "application/json"}
